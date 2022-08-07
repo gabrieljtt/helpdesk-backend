603f9eed24eb1fcebf96245c2de2ce44669ec277
gabrieljtt <gabriel_turatti@hotmail.com>
2022-08-07 16:57:43 -0300

    Repositories criados e carga inicial na base de dados

--- src/main/java/com/gabriel/helpdesk/domain/Cliente.java
+++ src/main/java/com/gabriel/helpdesk/domain/Cliente.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.gabriel.helpdesk.domain.enums.Perfil;
+
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
@@ -15,10 +17,12 @@ public class Cliente extends Pessoa {
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
