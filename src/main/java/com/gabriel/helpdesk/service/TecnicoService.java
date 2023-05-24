cdbef4550a422c3654901d343fef50e9ac7ef547
gabrieljtt <gabriel_turatti@hotmail.com>
2023-05-24 16:53:58 -0300

    Excecao para CPF e E-mail ja cadastrados

--- src/main/java/com/gabriel/helpdesk/service/TecnicoService.java
+++ src/main/java/com/gabriel/helpdesk/service/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gabriel.helpdesk.domain.Pessoa;
 import com.gabriel.helpdesk.domain.Tecnico;
 import com.gabriel.helpdesk.domain.dtos.TecnicoDTO;
+import com.gabriel.helpdesk.repositories.PessoaRepository;
 import com.gabriel.helpdesk.repositories.TecnicoRepository;
+import com.gabriel.helpdesk.service.exceptions.DataIntegrityViolationException;
 import com.gabriel.helpdesk.service.exceptions.ObjectNotFoundException;
 
 
@@ -17,6 +20,8 @@ public class TecnicoService {
 	
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -29,8 +34,23 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF ja cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail ja cadastrado no sistema!");
+		}
+		
 	}
 
 }
