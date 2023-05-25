340f866a432e59cf2bb61abef3e2c9cc84544ad6
gabrieljtt <gabriel_turatti@hotmail.com>
2023-05-24 22:20:06 -0300

    Validacao nos campos de criacao de novo usuario

--- src/main/java/com/gabriel/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gabriel/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import com.gabriel.helpdesk.service.exceptions.DataIntegrityViolationException;
 import com.gabriel.helpdesk.service.exceptions.ObjectNotFoundException;
@@ -29,6 +31,19 @@ public class ResourceExceptionHandler {
 				"Data breach", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Validation error", "Erro na validação dos campos", request.getRequestURI());
+		
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 
 }
