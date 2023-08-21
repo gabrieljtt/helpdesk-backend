ff1878346c1f5eae83f5d07044bc5f52b92f002e
gabrieljtt <gabriel_turatti@hotmail.com>
2023-08-21 19:23:16 -0300

    update para Chamado implementado

--- src/main/java/com/gabriel/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/gabriel/helpdesk/resources/ChamadoResource.java
@@ -11,15 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.helpdesk.domain.Chamado;
-import com.gabriel.helpdesk.domain.Cliente;
 import com.gabriel.helpdesk.domain.dtos.ChamadoDTO;
-import com.gabriel.helpdesk.domain.dtos.ClienteDTO;
 import com.gabriel.helpdesk.service.ChamadoService;
 
 @RestController
@@ -47,6 +46,12 @@ public class ChamadoResource {
 		Chamado obj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO){
+		Chamado newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
 	}
 
 }
